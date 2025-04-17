2599888c92b4fb4d40b06b3a5205c94196a264d5
unknown <uwishemaceline4@gmail.com>
2025-04-18 00:33:08 +0200

    dashboard ui

---
+++ src/components/dashboard/Main.jsx
@@ -0,0 +1,10 @@
+export default function Main() {
+  return (
+    <div>
+      <h1 className="text-gray-700 font-bold text-3xl">
+        Welcome to the dashboard
+      </h1>
+      <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6"></div>
+    </div>
+  );
+}
